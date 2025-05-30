b1cc5b68ed973ad74da6cfede5eec4405a249e7d
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-05-30 09:55:17 +0000

    code.py

--- code.py
+++ code.py
@@ -11,12 +11,61 @@ import stage
 
 def game_scene():
 
+    # displays the greeting message
     print("== PYDASH ==")
     print("Good Luck, Jumper.")
 
     while True:
+        # placeholder
         pass
 
+
+if __name__ == "__main__":
+    game_scene()
+
+
+    image_bank_background = stage.Bank.from_png(Screenshot 2025-05-28 140820.png)
+    background = stage.Grid(image_bank_background, 10, 8)
+    
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
+
+
+import ugame
+import stage
+
+def game_scene():
+    
+    image_bank_background = stage.Bank.from_bmp24("Screenshot 2025-05-28 140820 (1).bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+    
+    game = stage.Stage(ugame.display, 10)
+    game.layers = [background]
+    game.render_block()
+    
+    while True:
+         pass
+
+if __name__ == "__main__":
+    game_scene()
+
+
+
+    import ugame
+import stage
+
+def game_scene():
+    
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+    
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
+    
+    while True:
+         pass
 
 if __name__ == "__main__":
     game_scene()
